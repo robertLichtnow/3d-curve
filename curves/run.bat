70ac9e0f6d225bed3d69c433d2628c97046ab6b0
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-06-08 11:41:15 -0300

    Adicionados arquivos para automação de execução

---
+++ curves/run.bat
@@ -0,0 +1,2 @@
+mvn package -Dmaven.test.skip=true && ^
+java -cp target/curves-1.0-SNAPSHOT.jar edu.udc.cg.App
--- curves/run.sh
+++ curves/run.sh
@@ -1,5 +1,5 @@
 # Builds the project
-mvn package
+mvn package -Dmaven.test.skip=true
 
 # Runs the project
 java -cp target/curves-1.0-SNAPSHOT.jar edu.udc.cg.App
